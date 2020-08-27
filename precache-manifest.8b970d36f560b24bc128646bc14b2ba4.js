d68c047b693594e3c742d4d6d99280c9af0da051
markm7777 <mark.murphy7777@gmail.com>
2020-08-27 15:45:09 -0700

    Updates

---
+++ precache-manifest.8b970d36f560b24bc128646bc14b2ba4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f620f058ccb576f2e910e98769905bb",
+    "url": "/WinnetkaTodos/index.html"
+  },
+  {
+    "revision": "8c210827a73fbeff9031",
+    "url": "/WinnetkaTodos/static/css/main.a0219023.chunk.css"
+  },
+  {
+    "revision": "9f39bad862101c3d90b6",
+    "url": "/WinnetkaTodos/static/js/2.33009dad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/WinnetkaTodos/static/js/2.33009dad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c210827a73fbeff9031",
+    "url": "/WinnetkaTodos/static/js/main.76d2cf50.chunk.js"
+  },
+  {
+    "revision": "303b5c3ebdbaca13bf49",
+    "url": "/WinnetkaTodos/static/js/runtime-main.76b676a3.js"
+  }
+]);
